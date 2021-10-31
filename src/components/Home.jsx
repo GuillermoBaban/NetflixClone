e2724e77022641f7dcbb737a17036a3727472f5c
guillermoBabangit <guillermo.baban@hotmail.com>
2021-10-31 15:53:48 -0300

    slider netflix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,55 @@
 import { getTrending } from "../services/index";
-import { useEffect, useState } from "react";
-import { Row, Card, Container } from "react-bootstrap";
-import { useHorizontalScroll } from "../Hooks/useSideScroll";
+import { useEffect, useState, useRef } from "react";
+import { Row, Card, Container, Button } from "react-bootstrap";
 
-import "./HomeS.css";
+import "./styles.css";
 
 const URL = "https://www.themoviedb.org/t/p/w533_and_h300_bestv2";
 
 export const Home = () => {
-  const scrollRef = useHorizontalScroll();
   const [movies, setMovies] = useState([]);
+  const [arrowMove, setArrowMove] = useState(0);
 
   useEffect(() => {
-    getTrending().then((res) => {
-      setMovies(res);
-    });
+    getTrending().then((res) => setMovies(res));
   }, []);
+
+  const margin_left = useRef();
+
+  const handleRef = () => {
+    if (arrowMove === -1) {
+      margin_left.current.style.marginLeft = "0%";
+      margin_left.current.style.transition = "0.4s";
+    }
+    if (arrowMove === 0) {
+      margin_left.current.style.marginLeft = "-100%";
+      margin_left.current.style.transition = "0.4s";
+    }
+    setArrowMove(arrowMove + 1);
+    console.log(arrowMove);
+    if (arrowMove === 1) margin_left.current.style.marginLeft = "-200%";
+    else if (arrowMove === 2) {
+      margin_left.current.style.marginLeft = "-237%";
+      setArrowMove(-1);
+    }
+  };
 
   return (
     <Container fluid style={{ backgroundColor: "black", height: "100vh" }}>
       <h3 className="text-primary p-3">Trending</h3>
       <Row
+        ref={margin_left}
         className="flex-nowrap hiddenBar"
         style={{
           display: "flex",
           overflowY: "hidden",
           overflowX: "scroll",
         }}
-        ref={scrollRef}
       >
         {movies.map((card, index) => (
           <Card style={{ width: "20rem", backgroundColor: "black" }}>
             <Card.Img
+              className="imgHover"
               variant="bottom"
               style={{ width: "20rem" }}
               src={URL + card.backdrop_path}
@@ -39,6 +57,7 @@ export const Home = () => {
           </Card>
         ))}
       </Row>
+      <Button onClick={handleRef}> Hola </Button>
     </Container>
   );
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,24 @@
 import axios from "axios";
 
 const API_KEY = "6edbe7a47920c457071b6c619755d4ee";
-
 export async function getTrending() {
   return axios
     .get(
       `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}
+    `
+    )
+    .then((response) => {
+      const {
+        data: { results },
+      } = response;
+      return results;
+    });
+}
+
+export async function getPopular() {
+  return axios
+    .get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1
     `
     )
     .then((response) => {
