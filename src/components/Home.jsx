536918088f1e4f5e8ba678f96e928ac22ff83d24
guillermoBabangit <guillermo.baban@hotmail.com>
2021-10-30 21:21:02 -0300

    without carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import { getTrending } from "../services/index";
 import { useEffect, useState } from "react";
 import { Row, Card, Container } from "react-bootstrap";
+import { useHorizontalScroll } from "../Hooks/useSideScroll";
+
+import "./HomeS.css";
 
 const URL = "https://www.themoviedb.org/t/p/w533_and_h300_bestv2";
 
 export const Home = () => {
+  const scrollRef = useHorizontalScroll();
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -12,19 +16,18 @@ export const Home = () => {
       setMovies(res);
     });
   }, []);
-  console.log(movies);
 
   return (
     <Container fluid style={{ backgroundColor: "black", height: "100vh" }}>
       <h3 className="text-primary p-3">Trending</h3>
       <Row
-        className="flex-nowrap"
+        className="flex-nowrap hiddenBar"
         style={{
           display: "flex",
           overflowY: "hidden",
           overflowX: "scroll",
-          scrollbar: "none",
         }}
+        ref={scrollRef}
       >
         {movies.map((card, index) => (
           <Card style={{ width: "20rem", backgroundColor: "black" }}>
