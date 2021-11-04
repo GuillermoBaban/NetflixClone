cd88e4984cfe6af91cf2647e874b1c827f25b842
guillermoBabangit <guillermo.baban@hotmail.com>
2021-11-04 19:18:24 -0300

    adding banner custom hook

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,38 +1,11 @@
-import axios from "../services/axios";
-import { useEffect, useState } from "react";
+import BannerHook from "../costomHooks/bannerHook";
 import { Spinner, Button } from "react-bootstrap";
-
 export const Banner = ({ fetchUrl }) => {
   const img_url = "https://image.tmdb.org/t/p/original";
 
-  const [movies, setMovies] = useState([]);
-  const [load, setLoad] = useState(false);
+  const { movies, load } = BannerHook(fetchUrl);
 
-  // Siempre tengo el mismo fetch
-
-  useEffect(() => {
-    async function getFetch() {
-      await axios
-        .get(fetchUrl)
-        .then((response) => {
-          setMovies(response);
-          setLoad(true);
-        })
-        .catch(async () => {
-          await axios
-            .get(
-              `https://api.themoviedb.org/3/movie/11?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-            )
-            .then((r) => {
-              setMovies(r);
-              setLoad(true);
-            });
-        });
-    }
-    getFetch();
-  }, [fetchUrl]);
-  console.log(movies);
-
+  console.log(movies, load);
   return (
     <>
       {load ? (
---
+++ src/costomHooks/bannerHook.js
@@ -0,0 +1,34 @@
+import axios from "../services/axios";
+import { useState, useEffect } from "react";
+
+const BannerHook = (fetchUrl) => {
+  const [movies, setMovies] = useState([]);
+  const [load, setLoad] = useState(false);
+
+  // Siempre tengo el mismo fetch
+
+  useEffect(() => {
+    async function getFetch() {
+      await axios
+        .get(fetchUrl)
+        .then((response) => {
+          setMovies(response);
+          setLoad(true);
+        })
+        .catch(async () => {
+          await axios
+            .get(
+              `https://api.themoviedb.org/3/movie/11?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+            )
+            .then((r) => {
+              setMovies(r);
+              setLoad(true);
+            });
+        });
+    }
+    getFetch();
+  }, [fetchUrl]);
+  return { movies, load };
+};
+
+export default BannerHook;
